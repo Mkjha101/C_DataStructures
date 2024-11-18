a0b7123dae167056fe133f2b6da3ee02f83c2337
Mkjha101 <mkjha.uietofficial@gmail.com>
2024-11-18 21:20:57 +0530

    Sep 3 Pratical Programs

---
+++ September 3/Output Images/P1.cpp
@@ -0,0 +1,13 @@
+/*
+as we know that we can't increase the size of the array 
+until we use vector. but using vector is not optimising 
+whereas firstly linked list is a dynamic data structure 
+where no memory location happens , grow/shrink run time 
+secondly insertion and deletion in linked list is easy.
+
+There is no need of continuous memory allocation, 
+and it will work upon run time.
+
+Types of linked list 
+1. singly ll 2. doubly ll 3. circular ll 4. circular doubly ll
+*/
